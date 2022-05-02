627716e3bcc054f45c2a8ebdc2b5530e6ecba3ab
akgvn <akgvn@yandex.com>
2022-05-03 01:59:55 +0300

    Remove unneeded function prototypes

--- src/math_functions.d
+++ src/math_functions.d
@@ -639,37 +639,6 @@ void Copy_Vector(double* d, double* s, int n)
 //    QR_Hessenberg_Matrix                                                    //
 ////////////////////////////////////////////////////////////////////////////////
 
-import core.stdc.math;
-
-
-//                        Internally Defined Routines
-static void   One_Real_Eigenvalue(double[] Hrow, double[] eigen_real,
-                                             double[] eigen_imag, int row, double shift);
-static void   Two_Eigenvalues(double* H, double* S, double[] eigen_real,
-                                            double[] eigen_imag, int n, int k, double t);
-static void   Update_Row(double* Hrow, double cos, double sin, int n, int k);
-static void   Update_Column(double* H, double cos, double sin, int n, int k);
-static void   Update_Transformation(double* S, double cos, double sin,
-                                                                                    int n, int k);
-static void   Double_QR_Iteration(double* H, double* S, int row, int min_row,
-                                                      int n, double* shift, int iteration);
-static void   Product_and_Sum_of_Shifts(double* H, int n, int max_row,
-                          double* shift, double* trace, double* det, int iteration);
-static int    Two_Consecutive_Small_Subdiagonal(double* H, int min_row,
-                                          int max_row, int n, double trace, double det);
-static void   Double_QR_Step(double* H, int min_row, int max_row, int min_col,
-                                             double trace, double det, double* S, int n);
-static void   Complex_Division(double x, double y, double u, double v,
-                                                                         double* a, double* b);
-static void   BackSubstitution(double* H, double[] eigen_real,
-                                                                  double[] eigen_imag, int n);
-static void   BackSubstitute_Real_Vector(double* H, double[] eigen_real,
-                      double[] eigen_imag, int row,  double zero_tolerance, int n);
-static void   BackSubstitute_Complex_Vector(double* H, double[] eigen_real,
-                      double[] eigen_imag, int row,  double zero_tolerance, int n);
-static void   Calculate_Eigenvectors(double* H, double* S, double[] eigen_real,
-                                                                  double[] eigen_imag, int n);
-
 ////////////////////////////////////////////////////////////////////////////////
 //  int QR_Hessenberg_Matrix( double* H, double* S, double[] eigen_real,      //
 //                     double[] eigen_imag, int n, int max_iteration_count)   //
