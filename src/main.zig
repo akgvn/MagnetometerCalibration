ccb074376ac4cb1ace0881d647c8a5d5527aeedb
akgvn <akgvn@yandex.com>
2022-05-03 18:57:28 +0300

    Try using C from Zig

--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,35 @@
 const std = @import("std");
-
-const Matrix = struct {
-    data: []f64,
-    cols: u64,
-    rows: u64,
-
-    fn init(cols: u64, rows: u64) Matrix {
-        return .{ .data = null, .cols = cols, .rows = rows };
-    }
-};
-
-const Result = struct { bias: [3]f64, corr: [9]f64 };
+const math = @import("math");
+const Matrix = @import("types.zig").Matrix;
+const Result = @import("types.zig").Result;
+const mapi = @import("math_api.zig");
 
 fn calculate_the_thing() Result {
     return .{ .bias = .{ 0.0, 0.0, 0.0 }, .corr = .{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 } };
 }
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
-
-test "basic test" {
-    const result = calculate_the_thing();
-
-    const combined_bias = [_]f64{ -0.021659, 0.013250, -0.026167 };
-
-    const correction = [_]f64{
+    var mat = [_]f64{
         0.956973,  -0.017809, 0.006564,
         -0.017809, 0.964533,  0.003304,
         0.006564,  0.003304,  1.036145,
     };
 
-    try std.testing.expectEqual(result, .{ .bias = combined_bias, .corr = correction });
+    var res: [9]f64 = .{0} ** 9;
+
+    mapi.Matrix_x_Its_Transpose(&res, &mat, 3, 3);
+
+    std.log.info("All your codebase are belong to us. {any}", .{res});
+}
+
+test "Mag.txt results test" {
+    const result = calculate_the_thing();
+
+    const expected = Result{ .bias = [_]f64{ -0.021659, 0.013250, -0.026167 }, .corr = [_]f64{
+        0.956973,  -0.017809, 0.006564,
+        -0.017809, 0.964533,  0.003304,
+        0.006564,  0.003304,  1.036145,
+    } };
+
+    try std.testing.expectEqual(expected, result);
 }
